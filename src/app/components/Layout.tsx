72b56d26415bdd26f48e3d3bf42b7edc073bc929
VARUN-009 <varunpunia009@gmail.com>
2025-04-27 16:30:33 +0530

    Try-2

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ReactNode } from "react";
 import Link from "next/link";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 interface LayoutProps {
   children: ReactNode;
